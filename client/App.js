5fb7ca76dce4836acb3667d5635fb8529041aee8
erinafir <140061244+erinafir@users.noreply.github.com>
2024-08-09 17:18:41 +0700

    feat:onboarding done

--- client/App.js
+++ client/App.js
@@ -1,29 +1,54 @@
 // App.js
-import * as React from 'react';
-import { View, Text, StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
+import * as React from "react";
+import { View, Text, StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
+import OnboardingScreen from "./screens/OnboardingScreen";
+import * as SecureStore from "expo-secure-store";
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
     </View>
   );
 }
 
 const Stack = createNativeStackNavigator();
+export const OnboardContext = React.createContext(null);
 
 function App() {
+  const [isAppFirstLaunched, setIsAppFirstLaunched] = React.useState(null);
+  React.useEffect(() => {
+    async function checkOnboard() {
+      const onboard = await SecureStore.getItemAsync("userOnboarded");
+      if (onboard) setIsAppFirstLaunched(true);
+    }
+    checkOnboard();
+  }, []);
   return (
-    <NavigationContainer>
-      <StatusBar style="auto" />
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <OnboardContext.Provider value={{ setIsAppFirstLaunched }}>
+      <NavigationContainer >
+        <StatusBar style="auto" />
+        {isAppFirstLaunched? (<Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>) : (<Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>)}
+      </NavigationContainer>
+    </OnboardContext.Provider>
   );
 }
 
